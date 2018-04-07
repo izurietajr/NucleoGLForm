9bcbfa7d82969c72a46a129131f689dd4ada7947
Jesus Izurieta <izurietajr@hotmail.com>
2018-04-07 02:47:32 -0400

    clean

--- flisol/admin.py
+++ flisol/admin.py
@@ -7,4 +7,3 @@ from flisol.models import Person
 @admin.register(Person)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'subject', 'email', 'phone')
-    # pass
--- flisol/views.py
+++ flisol/views.py
@@ -3,18 +3,15 @@ from django.shortcuts import render
 from django.urls.base import reverse_lazy
 from django.views.generic.base import TemplateView
 
-from django.views.generic.edit import FormView, CreateView
+from django.views.generic.edit import CreateView
 
 from flisol.forms import PersonForm
 
 
 class PersonView(CreateView):
 	form_class = PersonForm
-	template_name = 'person/index.html' # personform
+	template_name = 'person/registry.html'
 	success_url = reverse_lazy('flisol:thank_you')
-
-	# def get_form(self, form_class=None):
-	# 	return self.form_class(data=self.request.POST)
 
 
 class ThankYouView(TemplateView):
